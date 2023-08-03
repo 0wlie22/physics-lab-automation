c7a289a898e262e2b7ecded83c8d000a3efb9620
TheCrabilia <69972734+TheCrabilia@users.noreply.github.com>
2023-08-03 22:27:49 +0300

    Add student coefficient calculation function

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import numpy as np
+from pathlib import Path
 
-def count_error(data, check_rude_values):
+import numpy as np
+import scipy
+
+
+def count_error(data: list[float, ...], *, check_rude_values: bool = False):
     # Finding the average
     average = np.average(data)
 
@@ -8,17 +12,18 @@ def count_error(data, check_rude_values):
     squared_error = 0
     for i in data:
         squared_error += (i - average) ** 2
-    squared_error = (squared_error/(len(data) * (len(data) - 1)) ) ** 0.5
+    squared_error = (squared_error / (len(data) * (len(data) - 1))) ** 0.5
 
     if not check_rude_values:
         data, found_rude_value = delete_rude_values(data, squared_error, average)
         if found_rude_value:
-            average, squared_error = count_error(data, True)
+            average, squared_error = count_error(data, check_rude_values=True)
             return average, squared_error
 
     return average, squared_error
 
-def delete_rude_values(data, squared_error, average):
+
+def delete_rude_values(data: list[float, ...], squared_error: float, average: float):
     found_rude_value = False
 
     for i in data:
@@ -32,7 +37,7 @@ def format_template(**kwargs):
     values = kwargs
     formatted_template = "<div align=right>Darja Sedova, 221RDB030</div>"
 
-    with open("template.md", "r") as f:
+    with Path.open("template.md") as f:
         symbol = f.read(1)
 
         while symbol:
@@ -46,7 +51,7 @@ def format_template(**kwargs):
                     while next_symbol != ">":
                         key += next_symbol
                         next_symbol = f.read(1)
-                    
+
                     formatted_template += str(values.get(key))
             else:
                 formatted_template += symbol
@@ -56,15 +61,36 @@ def format_template(**kwargs):
     return formatted_template
 
 
-def main():
-    data = [1.224,1.333,1.196,1.273,1.220,1.321,1.208,1.212,1.230,1.205]
-    average, squared_error = count_error(data, False)
+def get_student_coef(n: int, cp: float = 0.95) -> float:
+    """Get student coefficient for specified measurement count and confidence probability.
+
+    Args:
+        n (int): measurement count
+        cp (float): confidence probability
+
+    Returns:
+        float: student coefficient
+    """
+    return scipy.stats.t.ppf((1 + cp) / 2, n - 1)
+
+
+def main() -> None:  # noqa: D103
+    data = [1.224, 1.333, 1.196, 1.273, 1.220, 1.321, 1.208, 1.212, 1.230, 1.205]
+    average, squared_error = count_error(data)
     absolute_error = squared_error * 2.57
     relative_error = absolute_error / average * 100
 
-    print(format_template(average=f"{average:.2f}",
-                          squared_error=f"{squared_error:.3f}",absolute_error=f"{absolute_error:.2f}",relative_error=f"{relative_error:.2f}",length=len(data), value="\gamma"))
+    print(
+        format_template(
+            average=f"{average:.2f}",
+            squared_error=f"{squared_error:.3f}",
+            absolute_error=f"{absolute_error:.2f}",
+            relative_error=f"{relative_error:.2f}",
+            length=len(data),
+            value=r"\gamma",
+        ),
+    )
+
 
 if __name__ == "__main__":
     main()
-
